a1d18ebc8f163860eff7d0ea6ed65ec3c12dcbb1
lornaz <lorna@Lornas-Mac-mini.local>
2015-10-09 09:23:27 -0700

    removed extra code to submit for bug report

--- meteorProductCapture.js
+++ meteorProductCapture.js
@@ -1,7 +1,6 @@
 function onDeviceReady() {
   // As an example, you now have the device name, Cordova version, etc. available
   alert('Device Cordova: ' + device.cordova);
-  // Now call one of your barcode functions, etc.
   
   	cordova.plugins.barcodeScanner.scan(
 	function (result) {
@@ -35,9 +34,21 @@ if (Meteor.isCordova) {
   Template.scanBarcode.events({
     'click button': function () {
     // scanner is not calling me back. I am getting called back to the ondeviceready
-   
-    	document.addEventListener("deviceready", onDeviceReady, false);
-     
+  	cordova.plugins.barcodeScanner.scan(
+	function (result) {
+          alert("We got a barcode\n" +
+            "Result: " + result.text + "\n" +
+            "Format: " + result.format + "\n" +
+            "Cancelled: " + result.cancelled);
+            if (!result.cancelled) {
+		      	 Session.set("barcode", result.text);
+		      	 }
+        }, 
+        function (error) {
+          alert("Scanning failed: " + error);
+        }
+
+	   );     
     }
    });
   
@@ -48,54 +59,11 @@ if (Meteor.isClient) {
 
   Meteor.startup(function () {
     // code to run on client at startup
-    // Wait for Cordova to load
-	document.addEventListener("deviceready", onDeviceReady, false);
+    // Wait for Cordova to load and run barcode scanner once to see it works from start-up. 
+	//document.addEventListener("deviceready", onDeviceReady, false);
 		
   });
   
-
-//Photo capture code
-  Template.takePackagePhoto.helpers({
-    photo1: function () {
-      return Session.get("photo1");
-    }
-  });
-
- Template.takePackagePhoto.events({
-    'click button': function(event, template) {
-        var cameraOptions = {
-            width: 600,
-            height: 600
-        };
-        MeteorCamera.getPicture(cameraOptions, function (error, data) {
-           if (!error) {
-               Session.set("photo1", data);
-           }
-        });
-        event.preventDefault();
-    }
-});
-
- Template.takeIngredientsPhoto.helpers({
-    photo2: function () {
-      return Session.get("photo2");
-    }
-  });
-
- Template.takeIngredientsPhoto.events({
-    'click button': function(event, template) {
-        var cameraOptions = {
-            width: 600,
-            height: 600
-        };
-        MeteorCamera.getPicture(cameraOptions, function (error, data) {
-           if (!error) {
-               Session.set("photo2", data);
-           }
-        });
-        event.preventDefault();
-    }
-});
 }
 
 if (Meteor.isServer) {
